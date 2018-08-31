6fe9fd1a5e2029e9d5b94d5a16bf1c222b3b2b5d
Samvid Jhaveri <samvid95@gmail.com>
2018-08-30 20:24:15 -0700

    I have simple enemies scripts are ready and punching is also working.

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -7,9 +7,13 @@ public class PlayerMovementController : PhysicsObject {
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
 
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
 	}
 
     protected override void ComputeVelocity()
@@ -30,6 +34,17 @@ public class PlayerMovementController : PhysicsObject {
                 velocity.y = velocity.y * 0.5f;
             }
         }
+
+        Debug.Log(move.x);
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < -0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+      //  animator.SetBool("grounded", grounded);
+    //   animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 
         targetVelocity = move * maxSpeed;
 
