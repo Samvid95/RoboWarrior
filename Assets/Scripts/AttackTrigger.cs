9f12c0db482d0d4a5e888d4de7dc324b229ebe3e
Samvid Jhaveri <samvid95@gmail.com>
2018-09-01 01:39:04 -0700

    Animations are done now. Working towards making a better map and game polishing. Still has to rewrite some stuff in the enemy scripts.

--- Assets/Scripts/AttackTrigger.cs
+++ Assets/Scripts/AttackTrigger.cs
@@ -7,6 +7,11 @@ public class AttackTrigger : MonoBehaviour {
     public int dmg = 20;
     bool lookingRight = true;
 
+    private bool attacking = false;
+
+    private float onTimer = 0.0f;
+    private float onTimerCd = 0.2f;
+
     private void Awake()
     {
         PlayerMovementController.OnFlip += ChangePosition;
@@ -21,22 +26,45 @@ public class AttackTrigger : MonoBehaviour {
     {
         if(collision.isTrigger == true && collision.CompareTag("Enemy") )
         {
-            collision.SendMessageUpwards("Damage", dmg);
+            attacking = true;
+            onTimer = onTimerCd;
+            
         } 
         
        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (attacking && collision.isTrigger == true && collision.CompareTag("Enemy"))
+        {
+            if (onTimer > 0)
+            {
+                onTimer -= Time.deltaTime;
+            }
+            else
+            {
+                collision.SendMessageUpwards("Damage", dmg);
+                onTimer = 0.5f;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        attacking = false;
     }
 
     void ChangePosition()
     {
         if (lookingRight)
         {
-            this.transform.position -= new Vector3(2.152f, 0, 0);
+            this.transform.position -= new Vector3(1.142f, 0, 0);
             lookingRight = false;
         }
         else
         {
-            this.transform.position += new Vector3(2.152f, 0, 0);
+            this.transform.position += new Vector3(1.142f, 0, 0);
             lookingRight = true;
         }
     }
---
+++ Assets/Scripts/Enemy/BulletScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+    public int dmg = 20;
+
+    public bool spawnSpikey;
+
+    public GameObject Spikey; 
+     
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //This is for the bullet! 
+        if (collision.isTrigger == false && collision.CompareTag("Player"))
+        {
+            collision.SendMessageUpwards("Damage", dmg);
+            Destroy(gameObject);
+        }
+        if(collision.isTrigger == false && collision.CompareTag("Ground"))
+        {
+            if (spawnSpikey)
+            {
+                //Spawn a gameobject at this position!!!
+                Instantiate(Spikey, transform.position + new Vector3(0, 0.4f,0), Quaternion.identity);
+            }
+
+            Destroy(gameObject);
+        }
+        
+
+    }
+}
